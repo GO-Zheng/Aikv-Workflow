7de0855c44f4cdbcda865faa46a8c3849517c9b0
GO-Zheng <17817998233@163.com>
2026-04-20 16:36:39 +0800

    feat:2026-04-20 by wq

--- scripts/simple.sh
+++ scripts/simple.sh
@@ -30,12 +30,13 @@ h2() { info "${LIGHTCYAN}--- $* ---${NC}"; }
 
 # 使用说明
 usage() {
-    echo "Usage: $0 [-i|--image] [-c|--cluster] [-m|--monitor] [-d|--dir] [-b|--build]"
+    echo "Usage: $0 [-i|--image] [-c|--cluster] [-m|--monitor] [-d|--dir] [-b|--build] [-e|--expand]"
     echo "  -i, --image     同步镜像到远程节点"
     echo "  -c, --cluster   使用集群模式部署 (默认: 单节点模式)"
     echo "  -m, --monitor   部署监控节点"
     echo "  -d, --dir       同步文件到远程节点"
     echo "  -b, --build     重新构建 Docker 镜像 (build_docker.sh --dev --cluster)"
+    echo "  -e, --expand    仅部署扩容节点 (master-3, replica-3a, replica-3b)"
     exit 1
 }
 
@@ -45,8 +46,9 @@ CLUSTER_MODE=false
 DEPLOY_MONITOR=false
 SYNC_FILES=false
 BUILD_IMAGE=false
+EXPAND_MODE=false
 
-OPTS=$(getopt -o "icmdb" -l "image,cluster,monitor,dir,build" -n "$0" -- "$@")
+OPTS=$(getopt -o "icmdbe" -l "image,cluster,monitor,dir,build,expand" -n "$0" -- "$@")
 if [[ $? -ne 0 ]]; then
     usage
 fi
@@ -59,6 +61,7 @@ while true; do
         -m|--monitor)    DEPLOY_MONITOR=true; shift ;;
         -d|--dir)        SYNC_FILES=true; shift ;;
         -b|--build)      BUILD_IMAGE=true; SYNC_IMAGES=true; shift ;;
+        -e|--expand)     EXPAND_MODE=true; CLUSTER_MODE=true; shift ;;
         --)              shift; break ;;
         *)               usage ;;
     esac
@@ -94,6 +97,18 @@ LOG_LEVEL=10
 # mapfile -t CLUSTER_IMAGES < <(docker compose -f "$DOCKER_DIR/docker-compose-cluster.yaml" config --images 2>/dev/null | sort -u)
 # mapfile -t MONITOR_IMAGES < <(docker compose -f "$DOCKER_DIR/docker-compose-monitor.yaml" config --images 2>/dev/null | sort -u)
 
+# 定义各模式部署的服务列表
+SINGLE_SERVICES="aikv"
+CLUSTER_SERVICES="cadvisor promtail aikv-master-1 aidb-exporter aikv-exporter aikv-replica-1a aikv-exporter-replica-1a aidb-exporter-replica-1a aikv-replica-1b aikv-exporter-replica-1b aidb-exporter-replica-1b aikv-master-2 aikv-exporter-master-2 aidb-exporter-master-2 aikv-replica-2a aikv-exporter-replica-2a aidb-exporter-replica-2a aikv-replica-2b aikv-exporter-replica-2b aidb-exporter-replica-2b"
+EXPAND_SERVICES="aikv-master-3 aikv-exporter-master-3 aidb-exporter-master-3 aikv-replica-3a aikv-exporter-replica-3a aidb-exporter-replica-3a aikv-replica-3b aikv-exporter-replica-3b aidb-exporter-replica-3b"
+
+if $EXPAND_MODE; then
+    SERVICES="$EXPAND_SERVICES"
+elif $CLUSTER_MODE; then
+    SERVICES="$CLUSTER_SERVICES"
+else
+    SERVICES="$SINGLE_SERVICES"
+fi
 
 # 复制文件到远程节点
 MONITOR="root@$MONITOR_HOST"
@@ -138,16 +153,13 @@ main() {
 
     ssh $server "docker rm -f \$(docker ps -q -f name=aikv-replica) \$(docker ps -q -f name=aikv-master) \$(docker ps -q -f name=aikv)" > /dev/null 2>&1 || true
 
-    # 根据服务器 IP 设置 SERVER_HOST 环境变量
-    local server_ip="${server#root@}"  # 提取 IP 部分 (去掉 root@ 前缀)
-    local server_host_val="$server_ip"
-    # 如果是 SERVER_HOST_2，使用对应的 IP
-    [[ "$server_ip" == "$SERVER_HOST_2" ]] && server_host_val="$SERVER_HOST_2"
+    # 提取服务器 IP
+    local server_host_val="${server#root@}"
 
-    info "部署服务 (SERVER_HOST=$server_host_val)"
-    ssh $server "SERVER_HOST=$server_host_val docker compose -f /root/AiKv-Workflow/docker/$SERVER_COMPOSE up -d"
+    info "部署服务 (SERVER_HOST=$server_host_val, SERVICES=$SERVICES)"
+    ssh $server "SERVER_HOST=$server_host_val docker compose -f /root/AiKv-Workflow/docker/$SERVER_COMPOSE up -d $SERVICES"
 
-    if $CLUSTER_MODE; then
+    if $CLUSTER_MODE && ! $EXPAND_MODE; then
         info "初始化集群 (SERVER_HOST=$server_host_val)"
         # 初始化集群时，需要传入正确的 master/replica 地址
         ssh $server "SERVER_HOST=$server_host_val CLUSTER_REDIS_CONNECT_HOST=$server_host_val ./AiKv-Workflow/docker/init.sh \
